3e53b547ecc28006063c25feb008c91f80aac290
Loorinho <romeojacobson@gmail.com>
2026-01-03 20:38:14 +0300

    moved away from the render function to an actual liveview file to render the html. Also enabled live reload by installing the inotify-tools package on my ubuntu machine

--- lib/shop_web/live/budget_list_live.ex
+++ lib/shop_web/live/budget_list_live.ex
@@ -12,28 +12,4 @@ defmodule ShopWeb.BudgetListLive do
 
     {:ok, socket}
   end
-
-  def render(assigns) do
-    ~H"""
-    <div class="container mx-auto p-4">
-      <h1>Budget List</h1>
-
-      <.table id="budgets-table" rows={@budgets}>
-        <:col :let={budget} label="Name">
-          {budget.name}
-        </:col>
-        <:col :let={budget} label="Description">
-          {budget.description}
-        </:col>
-        <:col :let={budget} label="Start Date">
-          {budget.start_date}
-        </:col>
-
-        <:col :let={budget} label="End Date">
-          {budget.end_date}
-        </:col>
-      </.table>
-    </div>
-    """
-  end
 end
